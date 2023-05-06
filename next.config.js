811b6243a32bbe9321a73fbc238ff8ed2a0b9b6d
Olanrewaju Bayode <lanrebayode77@gmail.com>
2023-05-06 18:57:59 +0100

    latest

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["lanre-nft-marketplace.infura-ipfs.io", "infura-ipfs.io"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import "@/styles/globals.css";
+import { WagmiConfig, createClient, configureChains, mainnet } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { polygon, optimism, arbitrum, goerli, sepolia } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import "@rainbow-me/rainbowkit/styles.css";
+import { VotingSystemProvider } from "@/Hooks/client";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, goerli, sepolia],
+  [publicProvider(), alchemyProvider({ apiKey: process.env.ALCHEMY_ID })]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  connectors,
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <WagmiConfig client={client}>
+      <RainbowKitProvider chains={chains} modalSize="compact">
+        <VotingSystemProvider>
+          <Component {...pageProps} />
+        </VotingSystemProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
